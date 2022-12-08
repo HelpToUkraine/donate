ad62d4025fd6ef99b6d5e8d630298bbd4cd4d4b6
kuntus1 <vlasenko.max@lll.kpi.ua>
2022-12-08 14:52:54 +0200

    media queries + burger menu + slider in modalwork

--- js/app.js
+++ js/app.js
@@ -1,15 +1,53 @@
 $(function() {
+
+    var work = $("[data-category]")
+    var article = $(".articles-col")
+    var j = 1;
+
+    // Set number to works 
+    work.each(function() {
+        $(this).attr("number", j);
+        j++
+    });
+
+    j = 1;
+    article.each(function() {
+        $(this).attr("number", j);
+        j++
+    });
+    
+
+    /* Burger Menu*/
+    let i = 0;
+    const burger = $(".burger")
+
+    burger.on("click", function() {
+        if (i % 2 == 0) {        
+            $(".burger_close").removeClass('hide')
+            $(".burger_open").addClass('hide')
+            $(".navigation").css("display", "flex")
+
+        } else {
+            $(".burger_open").removeClass('hide')
+            $(".burger_close").addClass('hide')
+            $(".navigation").css("display", "none")
+        }
+        i++;
+    });
+
+
     /* Filter */
    let filter = $("[data-filter]")
-    
     filter.on("click", function(event) {
         event.preventDefault();
         let category = $(this).data('filter')
 
         if (category == 'all') {
             $("[data-category]").removeClass('hide');
+            element.textContent = 'SHOW LESS WORK'
         } 
         else {
+            element.textContent = 'SHOW ALL WORK'
             $("[data-category]").each(function() {
                 let temp = $(this).data('category')
 
@@ -22,16 +60,85 @@ $(function() {
         }
     });
 
+    /* LoadMoreWork */
+    const load = $("#load")
+    const element = document.getElementById('load')
+
+    load.on("click", function() {
+        if (element.textContent == 'SHOW ALL WORK') {
+            AllShow();
+            element.textContent = 'SHOW LESS WORK'
+        }
+        else if (element.textContent == 'LOAD MORE WORK') {
+            /* WorkShow(); */
+            AllShow();
+            element.textContent = 'SHOW LESS WORK'
+        }
+        else {
+            WorkHide();
+            element.textContent = 'LOAD MORE WORK'
+        }
+    });
+
+
+    /* LoadMorePost */
+    const news = $("[data-news]")
+    const see_more = $("#see-more")
+    const see = document.getElementById('see-more')
+
+    see_more.on("click", function() {
+        if (see.textContent == 'See more posts') {
+            ShowNews();
+            see.textContent = 'See less posts'
+        } else {
+            HideNews();
+            see.textContent = 'See more posts'
+        }
+    })
+
+
     /* Modal */
     const modal = $("[data-modal]")
     const close = $("[data-close]")
+    let modalId, workId;
 
     modal.on("click", function(event) {
+       
+        console.log("click to modal")
         event.preventDefault();
-        let modalId = $(this).data('modal')
+        event.stopPropagation()
+    
+        modalId = $(this).data('modal') 
+        workId = $(this).attr('number')
+
+        console.log(modalId)
+        console.log($(this).attr('number'))
 
         $(modalId).addClass('show')
         $("body").addClass('no-scroll')
+
+//----------------SLIDER---------------------
+        const left = $(".modal-work_btn--left")
+        const right = $(".modal-work_btn--right")
+        let counter = 0
+
+        left.off().on("click", function(){
+            workId--
+            ShowNextModalWork()
+            
+        })
+
+        right.off().on("click", function(){
+            workId++
+            ShowNextModalWork()
+        })
+//----------------SLIDER---------------------
+
+        const hire = $('#hire-me')
+        hire.on("click", function(){
+            $("body").addClass('no-scroll')
+            $("#modal_hireme").addClass('show')
+        })
     });
 
     close.on("click", function(event) {
@@ -54,4 +161,46 @@ $(function() {
     });
 
 
+    function ShowNextModalWork() {
+        if (workId <= 0) {
+            workId = work.length
+        }
+        else if (workId > work.length) {
+            workId = 1
+        }
+        
+        let nextModal = $("[number='"+ workId +"']").data('modal')
+        
+        console.log("showed:" + nextModal + "workID "+ workId) 
+        $(nextModal).addClass('show')
+        counter = 0
+
+        if (nextModal != modalId)
+            $(modalId).removeClass('show')
+        modalId = nextModal
+    }
+    
+    function WorkHide() {
+        modal.each(function() {
+            if ($(this).data('modal') == '#modal_project') {
+                $(this).addClass('hide')
+            }
+        });
+    }
+
+    function AllShow() {
+        modal.removeClass('hide')
+    }
+
+    function HideNews() {
+        news.each(function() {
+            if ($(this).data('news') == '#news_project') {
+                $(this).addClass('hide')
+            }
+        })
+    }
+
+    function ShowNews() {
+        news.removeClass('hide')
+    }
 });
